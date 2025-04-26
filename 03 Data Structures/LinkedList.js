ec84812d64e2ecb0b0fb0a773408244cc9670ee2
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-04-26 22:46:46 +0300

    refactor: clean up commented console logs in LinkedList class

--- 03 Data Structures/LinkedList.js
+++ 03 Data Structures/LinkedList.js
@@ -13,28 +13,66 @@ class LinkedList {
   }
   append(value) {
     const newNode = new Node(value);
-    console.log("step 0:", this);
+    // console.log("step 0:", this);
     this.tail.next = newNode; // Connect current last node to new last node
-    console.log("step 1:", this);
+    // console.log("step 1:", this);
     this.tail = newNode; // Update tail reference to point to new last node
-    console.log("step 2:", this);
+    // console.log("step 2:", this);
     this.length++;
     // console.log(this);
     return this;
   }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      throw new Error("Index out of bounds");
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index === this.length) {
+      this.append(value);
+      return this.printList();
+    }
+    const newNode = new Node(value);
+    let targetNode = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      targetNode = targetNode.next;
+    }
+    newNode.next = targetNode.next;
+    targetNode.next = newNode;
+    this.length++;
+    return this.printList();
+  }
 
-  prepend(value) {}
+  remove(index) {}
 
-  printList() {}
-
-  // insert(index, value)
-  // remove(index)
   // get(index)
+  // printList() {}
   // reverse()
 }
 
 const myLinkedList = new LinkedList(1);
 myLinkedList.append(2);
 myLinkedList.append(3);
-// myLinkedList.append(4);
+myLinkedList.prepend(0);
+// console.log(myLinkedList.printList());
+myLinkedList.insert(1, 4);
+console.log(myLinkedList.printList());
 // console.log(myLinkedList);
