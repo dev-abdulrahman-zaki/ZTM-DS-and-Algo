14b0dde821743f896287743201ad5349ac287b3e
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-04-27 17:19:07 +0300

    add: implement Stack class with push, pop, peek, and isEmpty methods

---
+++ 03 Data Structures/Stack.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top?.value;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (this.length === 0) {
+      return null;
+    }
+    const currentTop = this.top;
+    if (this.length === 1) {
+      this.top = null;
+      this.bottom = null;
+      this.length--;
+      return currentTop.value;
+    }
+    this.top = this.top.next;
+    this.length--;
+    // return this;
+    return currentTop.value;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const myStack = new Stack();
+console.log(myStack.push("google"));
+console.log(myStack.push("google2"));
+console.log(myStack.push("google3"));
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
