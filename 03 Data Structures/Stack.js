0bde2309ec963a9b3d82a736805d516be5500b59
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-04-27 18:19:28 +0300

    add: implement StackUsingArray class with methods for push, pop, peek, and isEmpty

--- 03 Data Structures/Stack.js
+++ 03 Data Structures/Stack.js
@@ -47,7 +47,35 @@ class Stack {
   }
 }
 
-const myStack = new Stack();
+class StackUsingArray {
+  constructor() {
+    this.data = [];
+  }
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+  push(value) {
+    this.data.push(value);
+    return this;
+  }
+  pop() {
+    return this.data.pop();
+  }
+  isEmpty() {
+    return this.data.length === 0;
+  }
+}
+
+// const myStack = new Stack();
+// console.log(myStack.push("google"));
+// console.log(myStack.push("google2"));
+// console.log(myStack.push("google3"));
+// console.log(myStack.peek());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+
+const myStack = new StackUsingArray();
 console.log(myStack.push("google"));
 console.log(myStack.push("google2"));
 console.log(myStack.push("google3"));
