cd8158d9aa590a30df248a66b1ada55733efa39e
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-05-02 13:45:36 +0300

    refactor: clean up BinarySearchTree.js for improved readability

--- 03 Data Structures/BinarySearchTree.js
+++ 03 Data Structures/BinarySearchTree.js
@@ -10,23 +10,46 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    let childPosition = "";
+    while (currentNode !== null) {
+      if (value > currentNode.value) {
+        parentNode = currentNode;
+        childPosition = "right";
+        currentNode = currentNode.right;
+      } else {
+        parentNode = currentNode;
+        childPosition = "left";
+        currentNode = currentNode.left;
+      }
+    }
+
+    parentNode[childPosition] = newNode;
+    return this;
+  }
   lookup(value) {
     // return node or null
   }
-//   remove(value) {}
+  //   remove(value) {}
 }
 
 const tree = new BinarySearchTree();
-// tree.insert(9);
-// tree.insert(4);
-// tree.insert(6);
-// tree.insert(20);
-// tree.insert(170);
-// tree.insert(15);
-// tree.insert(1);
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
 // tree.remove(170);
-// JSON.stringify(traverse(tree.root));
+console.log(JSON.stringify(traverse(tree.root)));
 // console.log(tree.lookup(20));
 //     9
 //  4     20
