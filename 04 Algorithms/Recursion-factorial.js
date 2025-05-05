18cf655e0553a16cb20c68810fbdb0bcc233c9b8
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-05-05 22:13:42 +0300

    chore: no code changes made

--- 04 Algorithms/Recursion-factorial.js
+++ 04 Algorithms/Recursion-factorial.js
@@ -1,21 +1,18 @@
-function findFactorialRecursive(number) {
-  let answer = 1;
-  //  base case
-  if (number === 0) return answer;
-  answer *= number;
-  //  recursive case
-  return (answer *= findFactorialRecursive(number - 1));
-}
-
 function findFactorialIterative(number) {
   let answer = 1;
-  for (let i = number; i > 0; i--) {
-    answer *= i;
+  for (let i = 2; i <= number; i++) {
+    answer = answer * i;
   }
   return answer;
 }
 
-// 5 * 4 * 3 * 2 * 1
+function findFactorialRecursive(number) {
+  if (number === 2) {
+    return 2;
+  }
+  return number * findFactorialRecursive(number - 1);
+}
 
-console.log(findFactorialRecursive(5));
-// console.log(findFactorialIterative(5));
+findFactorialIterative(5);
+findFactorialRecursive(5);
+
