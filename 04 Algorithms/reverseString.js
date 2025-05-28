caa0eb4d885b9f9b75dee49d320d30b29f9875d8
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-05-28 23:37:02 +0300

    chore: no code changes made

--- 04 Algorithms/reverseString.js
+++ 04 Algorithms/reverseString.js
@@ -8,14 +8,22 @@ function reverseString(str) {
 
 console.log(reverseString("yoyo master"));
 
-
 let reversedStr = "";
-function reverseStringRecursive(str) {  
+function reverseStringRecursive(str) {
   if (reversedStr.length === str.length) {
     return reversedStr;
   }
   reversedStr += str[str.length - reversedStr.length - 1];
   return reverseStringRecursive(str);
+}
+
+// another solution:
+function reverseStringRecursive(str) { 
+  if (str === "") {
+    return "";
+  } else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
 }
 
 console.log(reverseStringRecursive("yoyo master"));
